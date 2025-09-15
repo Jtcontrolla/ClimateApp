f62b7c97418b80e2f8bcb74e812e0f970c65f386
Timothy-Jacob Miyenseigha Marvellous <timothyjacobmarvelous@gmail.com>
2025-09-15 11:55:37 +0100

    change again

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: Process.env.VITE_API_ID || "/ClimateApp",
+  base: "/ClimateApp",
 })
